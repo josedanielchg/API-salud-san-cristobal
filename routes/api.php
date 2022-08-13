d06f7707b52b153412e3567edbac0d54b9586e2d
josedanielchg <j.daniel.chg@gmail.com>
2022-08-13 02:29:23 -0400

    add exception

--- routes/api.php
+++ routes/api.php
@@ -21,11 +21,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::post('/test', function(){
-    return "HOLAAAAAAa";
-});
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/townships', [TownshipController::class, 'index']);
